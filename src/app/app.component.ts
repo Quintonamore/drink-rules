f247795cdf6044747090c0c5d46402408db45cb4
Quintonamore <teas98@gmail.com>
2017-11-13 21:38:46 -0800

    House cleaning :recycle:
    Moved all of the data pulling functions to GetUserDataService and sucessfully using it in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
+import { GetUserDataService } from './common/services/getUserData.service';
 
 @Component({
   selector: 'app-root',
@@ -13,9 +14,38 @@ export class AppComponent {
 
   public page: number = 1;
 
+  public userInfo: any = "";
+
+  public gameDocs: Array<any> = [];
+
   public userDocString: string = 'eRdXuU2swHSyyM8B64zD';
 
-  constructor(){
-    
+  constructor(private userDataService: GetUserDataService) {
+    this.getUserInfo();
+  }
+
+
+  public getUserInfo() {
+    this.userDataService.getUserInfo(this.userDocString).subscribe(data => {
+      // Assign the data to userInfo and then call the GameInfo function since the user info is finished loading
+      this.userInfo = data;
+      this.getGameInfo();
+    });
+  }
+
+  public getGameInfo() {
+    // Temporary array and temp GetUserDataService for whatever reason (JS Closure?)
+    let tmp_service = this.userDataService;
+    let tmp_array = [];
+
+    // Send a request for each game document the user has.
+    this.userInfo.games.forEach(function(value){
+      tmp_service.getGameData(value.id).subscribe(data => {
+        tmp_array.push(data);
+        console.log(data);
+      });
+    });
+
+    this.gameDocs = tmp_array;
   }
 }
--- src/app/game-component/game.component.ts
+++ src/app/game-component/game.component.ts
@@ -1,8 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from 'angularfire2/firestore';
-import { Observable } from 'rxjs/Observable';
 import { DrinkingGameObj } from '../common/objects/drinkingGameObj'; // Unused for now
-import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 /**
  * Game component that lists all of a users games that they have created.
@@ -11,65 +8,13 @@ import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
   selector: 'game-component',
   templateUrl: './game.component.html'
 })
-export class GameComponent implements OnInit{
+export class GameComponent {
 
-  private games: AngularFirestoreDocument<{}>;
-
-  private userCollectionReference: AngularFirestoreCollection<any>;
-
-  private user: Observable<{}>;
-
-  public userInfo: any = "";
-
+  @Input()
   public gameDocs: Array<any> = [];
 
-  // Angular Firestore db import
-  private datab: AngularFirestore;
+  constructor(){
 
-  @Input() documentStr: string;
-
-  @Input() username: string;
-
-  constructor(db: AngularFirestore){
-    // Assign the db from the constructor so that it can be used in ngOnInit
-    this.datab = db;
-  }
-
-  ngOnInit() {
-    this.getUserInfo();
-  }
-
-  /**
-   * Method to set up the Game Component by getting the DB information
-   * 
-   * TODO: Seperate this DB stuff into an Angular Service
-   */
-  public getUserInfo() {
-    let tst = this.datab.collection('users').doc(this.documentStr);
-    this.user = tst.valueChanges();
-    this.user.subscribe(data => {
-      console.log(data);
-      this.userInfo = data;
-      this.getGameNames(data);
-    });
-  }
-
-  /**
-   * Method to grab all of the list of games and game names from the Firebase DB
-   */
-  public getGameNames(data) {
-    // Initialize helper db reference & temp array
-    let tst = this.datab.collection('games');
-    let tmp_array = this.gameDocs;
-
-    // Grab all of the ids and put them in a
-    data.games.forEach(function(value){
-      tst.doc(value.id).valueChanges().subscribe(data => {
-        tmp_array.push(data);
-      });
-    });
-
-    this.gameDocs = tmp_array;
   }
 
   /**
